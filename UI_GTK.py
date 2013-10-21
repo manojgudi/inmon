0a7f1b78c6e898f130043a98c71a33a21a238063
Manoj Gudi <manoj.p.gudi@gmail.com>
2013-10-22 00:01:15 +0530

    added timer module code

--- UI_GTK.py
+++ UI_GTK.py
@@ -6,6 +6,7 @@ import subprocess as sp
 
 try:
 	import pygtk
+	import gobject
 	pygtk.require("2.0")
 except:
 	print "check pygtk deps"
@@ -22,52 +23,81 @@ class front_end:
 	""" This is front handler for appsearch feature"""
 	def __init__(self):
 		
-		# Set environment path
-		#import os
-		#path="/opt/py_finder/"
-		#os.chdir(path)
-		
 		#Set glade file
 		self.gladefile = "./UI.glade"
 		self.glade = gtk.Builder()
 		self.glade.add_from_file(self.gladefile)
 		
+		#Set window file
 		self.window=self.glade.get_object("inmon_window")
 		self.window.show_all()
 		self.window.connect("destroy",self.main_quit)
 		
-		'''
+		#Set Label 
+		self.seconds_label = self.glade.get_object("inmon_label2")
+		# Intialize Seconds
+		self.seconds_label.set_text('20')
+
+
 		## dic start
 		self.dic = {
-		"on_apps_searchbox_activate" : self.on_apps_searchbox_activate,
-		"gtk_main_quit" : self.main_quit, 
-		"on_apps_search_button_clicked" : self.on_apps_searchbox_activate,
-		"on_file_searchbox_activate" : self.on_file_searchbox_activate,
-		"on_file_search_button_clicked" : self.on_file_searchbox_activate,
-		"on_notebook_switch_page" : self.on_notebook_focus_tab,
-		"on_quit_button_clicked" : self.on_quit_button_clicked,
-		"on_quit_wo_onboard_clicked" : self.on_quit_wo_onboard_clicked
+		"on_okay_press" : self.on_okay_press
+
 		}
 		## End of dic
 		self.glade.connect_signals(self.dic)
-		'''
 
 	def main_quit(self,widget):
 		"""
 			Predefined callback.
 			Equivalent to self.quit()
-			I want my ram free of any infections after fucking with GTK....;)
 		"""                    
 		
 		print "Exiting..."
-
 		gtk.main_quit()
                 
 	def main(self):		
 	# Insert any code just before apps goes into gtk.main()
+		self.timer()
 		print 'main()'
 
-	def on_quit_button_clicked(self, widget):
-		### Quit whole application and onboard/florence instance
-		self.main_quit(widget)
+	
 
+	def on_okay_press(self, widget):
+		self.inmon_button = self.glade.get_object("inmon_button")
+		self.touch_success()
+
+		print 'pressed okay'
+
+	### TIMER CODE ###
+	def timer(self):
+		self.timer_id = gobject.timeout_add(1000,self.update_clock)
+
+	def update_clock(self):
+		if self.timer_id is not None:
+			seconds = self.seconds_label.get_text()
+			if len(seconds):
+				seconds = int(seconds)
+			else:
+				seconds = 10
+			
+			# Count backwards
+			seconds = seconds - 1
+			self.seconds_label.set_text(str(seconds))
+			print seconds
+
+			if seconds == 0:
+				self.timer_id = None
+				print "Preparing to copy different touch config files"
+				self.touch_failed()
+
+			return True
+		
+		return False
+	### END TIMER CODE ###
+
+	def touch_failed(self):
+		pass
+
+	def touch_success(self):
+		pass
